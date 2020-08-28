241093c93c7cd173c50a3f3a6b5c155499b19c72
Thomas Bradford <thom@kode4food.it>
2020-08-28 14:04:41 +0200

    the bdd capabilties are broken

--- tests/gc/test_gc.cpp
+++ tests/gc/test_gc.cpp
@@ -8,27 +8,33 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.h"
 
-TEST_CASE("Garbage collect an atomic value") {
+SCENARIO("Garbage collect an atomic value") {
   Type int_type = {
       .default_size = sizeof(int),
   };
 
+  // GIVEN A newly instantiated Garbage Collector
   GC *gc = GCInit();
+
+  // THEN The reference should not be null
   REQUIRE(gc != NULL);
 
+  // AND_WHEN An integer is allocated and pinned
   Ref *r = GCNew(gc, &int_type);
   RefPin(r);
   int *i1 = DEREF(r, int);
   *i1 = 42;
 
-  Size_t freed = GCCollect(gc);
-  REQUIRE(freed == 0);
+  // THEN It should not be collected
+  REQUIRE(GCCollect(gc) == 0);
 
+  // AND_THEN The value should be retained
   int *i2 = DEREF(r, int);
-  REQUIRE(*i2 == *i1);
   REQUIRE(*i2 == 42);
 
+  // AND_WHEN The integer is unpinned
   RefUnpin(r);
-  freed = GCCollect(gc);
-  REQUIRE(freed == sizeof(int));
+
+  // THEN It should be capable of being freed
+  REQUIRE(GCCollect(gc) == sizeof(int));
 }
