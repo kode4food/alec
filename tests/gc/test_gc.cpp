33d01cec4c448c877c94d30b5f551f0f40a8dbb2
Thomas Bradford <thom@kode4food.it>
2020-08-28 15:14:36 +0200

    I was doing it wrong

--- tests/gc/test_gc.cpp
+++ tests/gc/test_gc.cpp
@@ -13,28 +13,32 @@ SCENARIO("Garbage collect an atomic value") {
       .default_size = sizeof(int),
   };
 
-  // GIVEN A newly instantiated Garbage Collector
-  GC *gc = GCInit();
+  GIVEN("A newly instantiated Garbage Collector") {
+    GC *gc = GCInit();
+    REQUIRE(gc != NULL);
 
-  // THEN The reference should not be null
-  REQUIRE(gc != NULL);
+    WHEN("An integer is allocated and pinned") {
+      Ref *r = GCNew(gc, &int_type);
+      RefPin(r);
+      int *i1 = DEREF(r, int);
+      *i1 = 42;
 
-  // AND_WHEN An integer is allocated and pinned
-  Ref *r = GCNew(gc, &int_type);
-  RefPin(r);
-  int *i1 = DEREF(r, int);
-  *i1 = 42;
+      THEN("It should not be collected") {
+        REQUIRE(GCCollect(gc) == 0);
 
-  // THEN It should not be collected
-  REQUIRE(GCCollect(gc) == 0);
+        AND_THEN("It should be retained") {
+          int *i2 = DEREF(r, int);
+          REQUIRE(*i2 == 42);
+        }
+      }
 
-  // AND_THEN The value should be retained
-  int *i2 = DEREF(r, int);
-  REQUIRE(*i2 == 42);
+      AND_WHEN("It is unpinned") {
+        RefUnpin(r);
 
-  // AND_WHEN The integer is unpinned
-  RefUnpin(r);
-
-  // THEN It should be capable of being freed
-  REQUIRE(GCCollect(gc) == sizeof(int));
+        THEN("It should be capable of being freed") {
+          REQUIRE(GCCollect(gc) == sizeof(int));
+        }
+      }
+    }
+  }
 }
