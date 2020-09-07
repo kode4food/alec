edcb837e2fcffb384ab3f01373163c2700ebeaec
Thomas Bradford <thom@kode4food.it>
2020-09-07 14:43:07 +0200

    don't need the gernated file

--- tests/main_gen.cpp
+++
@@ -1,5 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-#include "/Users/tbradford/work/alec/src/gc/__tests__/gc.hpp"
-#include "/Users/tbradford/work/alec/src/gc/__tests__/log.hpp"
-#include "/Users/tbradford/work/alec/src/runtime/__tests__/runtime.hpp"
